414833a253c3ebc4f9840bc6ec55b30197ed2a14
llefranc01 <37572375+llefranc01@users.noreply.github.com>
2023-09-12 21:22:04 -0700

    commit

---
+++ create_model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pickle
+
+data = pd.read_csv('creditcard.csv')
+
+X = data.drop(columns=['Class', 'Time'])
+y = data['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=True)
+
+
+#random forest model
+
+randforest = RandomForestClassifier(max_depth=10)
+randforest = randforest.fit(X_train, y_train)
+y_predict = randforest.predict(X_test)
+
+print(accuracy_score(y_test, y_predict))
+
+filename = 'Random_Forest_model.sav'
+pickle.dump(randforest, open(filename, 'wb'))
